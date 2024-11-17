86de915d5fa8faa88bb07e45d399a765b44f11e6
Stephenca290 <135217719+Stephenca290@users.noreply.github.com>
2024-11-17 23:50:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ Follow these simple steps to interact with the chatbot:
 
 # This is the first API key input; no need to repeat it in the main function.
 api_key = st.text_input("Enter your Google API Key:", type="password", key="api_key_input")
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
 new_db = FAISS.load_local("faiss_index", embeddings,allow_dangerous_deserialization=True)
 def get_pdf_text(pdf_docs):
     text = ""
