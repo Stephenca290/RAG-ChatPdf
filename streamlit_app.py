a71247319bebb09ed9845092959cf072399daf9c
Stephenca290 <135217719+Stephenca290@users.noreply.github.com>
2024-11-17 17:08:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-!pip install -U langchain-community
+!pip install  langchain-community
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
