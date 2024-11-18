b5edef075f72efaad59c99cdb4591f32bef3e6f3
Stephenca290 <135217719+Stephenca290@users.noreply.github.com>
2024-11-18 11:37:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,25 +14,13 @@ st.set_page_config(page_title="Document Genie", layout="wide")
 
 
 st.markdown("""
-## Document Genie: Get instant insights from your Documents
-
-This chatbot is built using the Retrieval-Augmented Generation (RAG) framework, leveraging Google's Generative AI model Gemini-PRO. It processes uploaded PDF documents by breaking them down into manageable chunks, creates a searchable vector store, and generates accurate answers to user queries. This advanced approach ensures high-quality, contextually relevant responses for an efficient and effective user experience.
-
-### How It Works
-
-Follow these simple steps to interact with the chatbot:
-
-1. **Enter Your API Key**: You'll need a Google API key for the chatbot to access Google's Generative AI models. Obtain your API key https://makersuite.google.com/app/apikey.
-
-2. **Upload Your Documents**: The system accepts multiple PDF files at once, analyzing the content to provide comprehensive insights.
-
-3. **Ask a Question**: After processing the documents, ask any question related to the content of your uploaded documents for a precise answer.
+##RAG ChatPdf
 """)
 
 
 
 # This is the first API key input; no need to repeat it in the main function.
-api_key = st.text_input("Enter your Google API Key:", type="password", key="api_key_input")
+api_key = st.secrets("api_key")
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -75,17 +63,17 @@ def user_input(user_question, api_key):
     st.write("Reply: ", response["output_text"])
 
 def main():
-    st.header("AI clone chatbot💁")
+    
 
-    user_question = st.text_input("Ask a Question from the PDF Files", key="user_question")
+    user_question = st.text_input("Ask anything?", key="user_question")
 
     if user_question and api_key:  # Ensure API key and user question are provided
         user_input(user_question, api_key)
 
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True, key="pdf_uploader")
-        if st.button("Submit & Process", key="process_button") and api_key:  # Check if API key is provided before processing
+        pdf_docs = st.file_uploader("Upload your Pdf Files", accept_multiple_files=True, key="pdf_uploader")
+        if st.button("Submit", key="process_button") and api_key:  # Check if API key is provided before processing
             with st.spinner("Processing..."):
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
